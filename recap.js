1624e61af0c0b85668a56e901694e3a590bac6a6
Shahin-Mahmud98 <shahinmahmud412@gmail.com>
2021-08-24 11:41:14 +0600

    file added

---
+++ recap.js
@@ -0,0 +1,29 @@
+// // 1. let and const 
+// const hubby = 'omor sani ';
+// let phone = 'iphone 15';
+// phone = 'samsung galaxy s17';
+
+// // default parameter
+
+// // template string
+// const myNotes = `I am mojnu of ${hubby}. i have a ${phone}`
+// console.log(myNotes);
+
+// console.log(min([1, 3, 2])) ? 
+// function min(nums) { return Math.min(nums) }
+
+// const [a, b] = [1, 2, 3, 4, 45, 5];
+// console.log(a + b);
+
+// const { x, y, z } = { x: 1, y1: 2, z: 3 };
+// console.log(y);
+
+// const nums = [1, 2, 3, 4, 5]; let output = nums.filter(n => n % 2);
+// console.log(output);
+
+const friends = ["Moushumi", "Misha", "Manna", "mimi", "mahiya"];
+// friends.find(friend.length == 5);
+// friends.filter(friend => frirnd.length == 5);
+// friends.find(friend => frirnd.length == 5);
+
+friends.find(friend => friend == 5);
